3ca287e33543daa0609babdcd36e834b9d80a022
Paul Isaris <paulisaris@gmail.com>
2022-10-05 11:25:13 +0300

    Convert bundle to JSON

--- app/src/main/java/gr/scify/icsee/controllers/GeneralAnalyticsLogger.java
+++ app/src/main/java/gr/scify/icsee/controllers/GeneralAnalyticsLogger.java
@@ -9,6 +9,8 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Set;
+
 import gr.scify.icsee.ICSeeApplication;
 import gr.scify.icsee.data.LoginRepository;
 import gr.scify.icsee.interfaces.IAnalyticsLogger;
@@ -23,7 +25,7 @@ public class GeneralAnalyticsLogger implements IAnalyticsLogger {
             jsonObject.put("source", "icsee_mobile");
             jsonObject.put("action", eventName);
             jsonObject.put("value", eventValue);
-            jsonObject.put("payload", additionalPayload);
+            jsonObject.put("payload", bundleToJSON(additionalPayload));
             jsonObject.put("token", LoginRepository.getInstance().getStoredAuthToken(c));
         } catch (JSONException e) {
             e.printStackTrace();
@@ -35,5 +37,19 @@ public class GeneralAnalyticsLogger implements IAnalyticsLogger {
                 Throwable::printStackTrace
         );
         ICSeeApplication.queue.add(postRequest);
+    }
+
+    protected JSONObject bundleToJSON(Bundle bundle) {
+        JSONObject json = new JSONObject();
+        Set<String> keys = bundle.keySet();
+        for (String key : keys) {
+            try {
+                // json.put(key, bundle.get(key)); see edit below
+                json.put(key, bundle.get(key));
+            } catch (JSONException e) {
+                //Handle exception here
+            }
+        }
+        return json;
     }
 }
