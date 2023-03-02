245fb2aeea03c810c903c316f2972aae06ecb558
Paul Isaris <paulisaris@gmail.com>
2023-03-02 20:36:09 +0200

    Code cleanup

--- app/src/main/java/gr/scify/icsee/AsyncProgressCheck.java
+++ app/src/main/java/gr/scify/icsee/AsyncProgressCheck.java
@@ -1,5 +1,6 @@
 package gr.scify.icsee;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -10,30 +11,29 @@ import org.opencv.android.LoaderCallbackInterface;
 import gr.scify.icsee.camera.ModifiedLoaderCallback;
 
 public class AsyncProgressCheck extends AsyncTask<Void, Void, Void> {
-    ICSeeStartActivity mContext;
+    protected Activity mContext;
     ProgressDialog mDialog;
     ModifiedLoaderCallback mOpenCVCallBack;
     protected String TAG = ICSeeRealtimeActivity.class.getCanonicalName();
 
-    public AsyncProgressCheck (ProgressDialog pd, ModifiedLoaderCallback aa,ICSeeStartActivity cc) {
+    public AsyncProgressCheck(ProgressDialog pd, ModifiedLoaderCallback aa, Activity cc) {
         mDialog = pd;
         mOpenCVCallBack = aa;
-        mContext=cc;
+        mContext = cc;
     }
 
     @Override
     protected Void doInBackground(Void... params) {
-    	// Check for back camera
-    	// If not found
-    	if (gr.scify.icsee.camera.Utils.findFrontFacingCamera() == -1) {
-    		mContext.showErrorMessage("No back camera found.", true);
-    		return null;
-    	}
-    	// While there has been no return from OpenCV
+        // Check for back camera
+        // If not found
+        if (gr.scify.icsee.camera.Utils.findFrontFacingCamera() == -1) {
+            return null;
+        }
+        // While there has been no return from OpenCV
         while (mOpenCVCallBack.processStatus < 0) {
             try {
-            	// Wait for a while
-                Thread.sleep(500);
+                // Wait for a while
+                this.wait(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -41,32 +41,26 @@ public class AsyncProgressCheck extends AsyncTask<Void, Void, Void> {
         return null;
     }
 
-	@Override
+    @Override
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
-            switch (mOpenCVCallBack.processStatus) {
-                case LoaderCallbackInterface.SUCCESS: {
-                    Log.i(TAG, "OpenCV loaded successfully");
-                    // Prepare new intent
-                    Intent strt = new Intent(mContext,ICSeeRealtimeActivity.class);
-                    strt.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    mContext.startActivity(strt);
-                    mContext.finish();
-                    break;
-                }
-                case LoaderCallbackInterface.INSTALL_CANCELED:  {
-                    Log.i(TAG, "OpenCV installation cancelled by user.");
-                    mContext.showErrorMessage("OpenCV was not installed. Cannot continue.", 
-                    		true);
-                    return;
-                }
-                default: {
-                    Log.i(TAG, "OpenCV didn't load successfully");
-                    mContext.showErrorMessage("OpenCV didn't load successfully. Cannot continue.", 
-                    		true);
-                }
+        switch (mOpenCVCallBack.processStatus) {
+            case LoaderCallbackInterface.SUCCESS: {
+                Log.i(TAG, "OpenCV loaded successfully");
+                // Prepare new intent
+                Intent intent = new Intent(mContext, ICSeeRealtimeActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                mContext.finish();
+                mContext.startActivity(intent);
+                break;
+            }
+            case LoaderCallbackInterface.INSTALL_CANCELED: {
+                Log.i(TAG, "OpenCV installation cancelled by user.");
+                return;
+            }
+            default: {
+                Log.i(TAG, "OpenCV didn't load successfully");
+            }
         }
-
-        return;
     }
 }
