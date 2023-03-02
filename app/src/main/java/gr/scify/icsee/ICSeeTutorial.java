92996aa683cca0f0d87659872270006d9bad9538
Paul Isaris <paulisaris@gmail.com>
2023-03-02 20:00:16 +0200

    Improved context Fixed MediaPlayer initialization

--- app/src/main/java/gr/scify/icsee/ICSeeTutorial.java
+++ app/src/main/java/gr/scify/icsee/ICSeeTutorial.java
@@ -11,7 +11,7 @@ public class ICSeeTutorial {
     private static final String KEY_TUTORIAL = "key_tutorial";
     private static SharedPreferences.Editor mEditor;
     protected static String TAG = ICSeeTutorial.class.getCanonicalName();
-    private static MediaPlayer mediaPlayer = new MediaPlayer();
+    private static MediaPlayer mediaPlayer;
 
     public static void tutorialOn() {
         mEditor.putInt(KEY_TUTORIAL, 1);
@@ -34,7 +34,7 @@ public class ICSeeTutorial {
     }
 
     public static void playTutorialReminder(Context context) {
-        if (getTutorialState(ICSeeRealtimeActivity.mContext) == 0) {
+        if (getTutorialState(context) == 0) {
             startPLayer(context, R.raw.tutorial_reminder);
         } else {
             playDragFingerTutorial(context);
@@ -54,43 +54,43 @@ public class ICSeeTutorial {
     }
 
     public static void playAdjustZoom(Context context) {
-        if (getTutorialState(ICSeeRealtimeActivity.mContext) == 1) {
+        if (getTutorialState(context) == 1) {
             startPLayer(context, R.raw.adjust_zoom);
         }
     }
 
     public static void playTakePictureReminder(Context context) {
-        if (getTutorialState(ICSeeRealtimeActivity.mContext) == 1) {
+        if (getTutorialState(context) == 1) {
             startPLayer(context, R.raw.take_picture);
         }
     }
 
     public static void playChangedFilter(Context context) {
-        if (getTutorialState(ICSeeRealtimeActivity.mContext) == 1) {
+        if (getTutorialState(context) == 1) {
             startPLayer(context, R.raw.changed_filter);
         }
     }
 
     public static void playDragFingerTutorial(Context context) {
-        if (getTutorialState(ICSeeRealtimeActivity.mContext) == 1) {
+        if (getTutorialState(context) == 1) {
             startPLayer(context, R.raw.drag_finger);
         }
     }
 
     public static void playNoFiltersLeft(Context context) {
-        if (getTutorialState(ICSeeRealtimeActivity.mContext) == 1) {
+        if (getTutorialState(context) == 1) {
             startPLayer(context, R.raw.no_filters_left);
         }
     }
 
     public static void playNoFiltersRight(Context context) {
-        if (getTutorialState(ICSeeRealtimeActivity.mContext) == 1) {
+        if (getTutorialState(context) == 1) {
             startPLayer(context, R.raw.no_filters_right);
         }
     }
 
     public static void playAutoFocus(Context context) {
-        if (getTutorialState(ICSeeRealtimeActivity.mContext) == 1) {
+        if (getTutorialState(context) == 1) {
             startPLayer(context, R.raw.auto_focus_tutorial);
         }
     }
